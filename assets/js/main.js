cb0b0ebe43310df8b1b3345dbfffd8e8374aba73
Graham Watson <higrahamwatson@gmail.com>
2022-06-10 17:52:54 -0400

    changes to menu and logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,8 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
+			xlarge:   [ '1681px',  '1880px' ],
+			large:    [ '981px',   '1680px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ '361px',   '480px'  ],
